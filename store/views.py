290a8f256983c6bb1f0eb3916e610d1542d33004
영구킴 <a097@097.local>
2020-12-11 04:01:03 +0900

    [modify]코드 다듬기

--- store/views.py
+++ store/views.py
@@ -1,3 +1,6 @@
+
+import random
+
 from django.views     import View
 from django.http      import JsonResponse
 from django.db.models import Q
@@ -6,50 +9,49 @@ from .models          import Product, Brand
 
 class ProductListView(View):
     def get(self, request):
-      
-        stores  = request.GET.getlist('store')
-        brands  = request.GET.getlist('brand')
-        types   = request.GET.getlist('type')
-        search  = request.GET.get('search')
-        page    = int(request.GET.get('page', 1))
+        brands     = request.GET.getlist('brand')
+        types      = request.GET.getlist('type')
+        search     = request.GET.get('search')
+        min_price  = request.GET.get('min_price')
+        max_price  = request.GET.get('max_price')
+        page       = int(request.GET.get('page', 1))
         
         q = Q()
 
-        if stores:
-            q &= Q(store__name__in=stores)
-
         if brands:
             q &= Q(brand__name__in=brands)
-
+            
         if types:
             q &= Q(product_type__name__in=types)
+        
+        if min_price and max_price:
+            q &= Q(market_price__range=(min_price,max_price))
 
         if search:
             q &= Q(brand__name__icontains=search) | Q(product_type__name__icontains=search) | Q(title__icontains=search)
         
-        products_list = Product.objects.filter(q)
+        products_list = Product.objects.select_related('product_type').prefetch_related('brand__product_set').filter(q)
         
-        page_size = 20
+        page_size = 21
         limit     = page * page_size
         offset    = limit - page_size
         products  = products_list[offset:limit]
 
-        if not page:
-            return JsonResponse({'message': 'PAGE_ERROR'}, status=400)
-        
-        products = Product.objects.filter(q)[:20]
-        
-        result = []
+        result = []  
 
         for product in products:
             result.append({
-                'title'        : product.title,
-                'market_price' : product.market_price,
+                'name'         : product.title,
+                'brand'        : product.brand.name,
+                'price'        : product.market_price,
                 'sale_price'   : product.sale_price,
                 'description'  : product.description,
                 'gender'       : product.gender.name,
-                'product_type' : product.product_type.name,
-                'product_img'  : product.thumnail_url            
+                'type'         : product.product_type.name,
+                'image'        : product.thumnail_url,
+                'count'        : product.brand.product_set.all().count(),
+                'heartCount'   : random.randint(50,200),
+                'commentCount' : random.randint(50,166)
             })
 
         return JsonResponse({'result': result}, status=200)
